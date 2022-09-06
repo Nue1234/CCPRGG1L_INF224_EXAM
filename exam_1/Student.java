6fc1daca0bcf37a9ed9c85d5d8c19b55793f128c
Sean Kervin Vidal Gumarao <111546618+Nue1234@users.noreply.github.com>
2022-09-06 09:33:10 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,37 @@
+public class Student {
+String Sname, Fname, Mi, DOB, SEA;
+int SN;
+boolean IAA;
+
+
+
+
+
+//hidden Student class
+public void sname() {
+    System.out.println();
+}
+public void fname() {
+}
+public void mi() {
+}
+public void dob() {
+}
+
+
+
+
+
+//Output
+public void sn() {
+    System.out.println("My student number: " + SN);
+}
+public void sea() {
+    System.out.println("Student's E-mail Address: " + SEA);
+}
+public void iaa() {
+    System.out.println("I am Awesome: " + IAA);
+}
+
+
+}
