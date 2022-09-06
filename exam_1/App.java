e1704ae5516b731a40f62db7cb6c1871135425c9
Sean Kervin Vidal Gumarao <111546618+Nue1234@users.noreply.github.com>
2022-09-06 09:32:48 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Student sean = new Student();
+
+        sean.Sname = "Gumarao";
+        sean.Fname = "Sean Kervin";
+        sean.Mi = "V";
+        sean.DOB = "12/20/1998";
+        sean.SN = 2022104926;
+        sean.SEA = "gumaraosv@students.national-u.edu.ph";
+        sean.IAA = true;
+
+        sean.sname();
+        sean.fname();
+        sean.mi();
+        sean.dob();
+        sean.sn();
+        sean.sea();
+        sean.iaa();
+
+
+    }
+}
